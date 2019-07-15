3c6961df8014facf625e77f655991002f98f1670
awstin17 <austinwiese.acw@gmail.com>
2019-07-14 22:26:43 -0700

    finished with second section

---
+++ src/components/Second-Section.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="container" class="container-fluid p-0">
+    <div class="row no-gutters" style="height: 500px">
+      <div class="col-3 offset-2 d-flex flex-column justify-content-center align-items-start">
+        <h1 class="font-weight-bold" style="color: #613476">Lorem Ipsum</h1>
+        <button style="background-color: #FF7218" class="btn text-white">BUTTON BUTTON</button>
+      </div>
+      <div class="col-6 offset-1"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class SecondSection extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#container {
+  background-image: url("../assets/banner-1.jpg");
+  background-size: cover;
+}
+</style>
