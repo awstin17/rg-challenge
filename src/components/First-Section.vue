0329db70fc39c1be4e30b0c1d05ebcbbc3190fdd
awstin17 <austinwiese.acw@gmail.com>
2019-07-16 10:11:34 -0700

    second section mobile view done

--- src/components/First-Section.vue
+++ src/components/First-Section.vue
@@ -6,7 +6,7 @@
         <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .</p>
         <a href style="color: #613476">> Lorem Ipsum</a>
       </div>
-      <div class="col-12 col-sm-6 offset-sm-1 pb-3 pb-sm-0">
+      <div class="col-12 col-sm-6 offset-sm-1 pb-5 pb-sm-0">
         <img style="background-size: cover" width="100%" src="../assets/img-1.jpg" />
       </div>
     </div>
--- src/components/Second-Section.vue
+++ src/components/Second-Section.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="container" class="container-fluid p-0">
-    <div class="row no-gutters" style="height: 600px">
-      <div class="col-3 offset-2 d-flex flex-column justify-content-center align-items-start">
-        <h1 class="font-weight-bold" style="color: #613476">Lorem Ipsum</h1>
+    <div class="row" style="height: 600px">
+      <div class="col-6 col-sm-3 offset-1 offset-sm-2 my-auto">
+        <h1 class="font-weight-bold" style="color: #613476">LOREM IPSUM</h1>
         <button style="background-color: #FF7218" class="btn text-white">BUTTON BUTTON</button>
       </div>
-      <div class="col-6 offset-1"></div>
     </div>
   </div>
 </template>
@@ -25,5 +24,6 @@ export default class SecondSection extends Vue {
   background-image: url("../assets/banner-1.jpg");
   background-size: cover;
   background-attachment: fixed;
+  background-position: 60%;
 }
 </style>
