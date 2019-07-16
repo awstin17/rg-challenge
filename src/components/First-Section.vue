9dc9790fa7baaf27ce1c1549071cd5020171ede4
awstin17 <austinwiese.acw@gmail.com>
2019-07-16 09:42:08 -0700

    finished first section header

--- src/components/First-Section.vue
+++ src/components/First-Section.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container-fluid p-0">
+  <div class="container-fluid p-sm-0">
     <div class="row no-gutters">
-      <div class="col-3 offset-2 d-flex flex-column justify-content-center">
+      <div class="col-12 col-sm-3 offset-sm-2 d-flex flex-column justify-content-center py-5">
         <h1 class="font-weight-bold" style="color: #613476">Lorem Ipsum</h1>
         <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .</p>
-        <a style="color: #613476">> Lorem Ipsum</a>
+        <a href style="color: #613476">> Lorem Ipsum</a>
       </div>
-      <div class="col-6 offset-1">
+      <div class="col-12 col-sm-6 offset-sm-1 pb-3 pb-sm-0">
         <img style="background-size: cover" width="100%" src="../assets/img-1.jpg" />
       </div>
     </div>
