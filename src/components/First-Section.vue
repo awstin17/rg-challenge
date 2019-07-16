747e5cb73c714a04e7e43671fc2b43fff4c3ace1
awstin17 <austinwiese.acw@gmail.com>
2019-07-16 13:33:17 -0700

    fixed first section image

--- src/components/First-Section.vue
+++ src/components/First-Section.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="container-fluid p-sm-0">
-    <div class="row no-gutters">
-      <div class="col-12 col-sm-3 offset-sm-2 d-flex flex-column justify-content-center py-5">
+  <div class="container-fluid p-0">
+    <div style="min-height: 526px" class="row no-gutters">
+      <div class="col-12 col-sm-3 offset-sm-2 d-flex flex-column justify-content-center py-5 px-2">
         <h1 class="font-weight-bold" style="color: #613476">Lorem Ipsum</h1>
         <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .</p>
         <a href style="color: #613476">> Lorem Ipsum</a>
       </div>
-      <div class="col-12 col-sm-6 offset-sm-1 pb-5 pb-sm-0">
-        <img style="background-size: cover" width="100%" src="../assets/img-1.jpg" />
+      <div style="min-height: 400px" id="test" class="col-12 col-sm-6 offset-sm-1 pb-5 pb-sm-0">
+        <!-- <img style="object-fit: cover; overflow: scroll" height="100%" src="../assets/img-1.jpg" /> -->
       </div>
     </div>
   </div>
@@ -24,4 +24,8 @@ export default class FirstSection extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#test {
+  background-image: url("../assets/img-1.jpg");
+  background-position: 50%;
+}
 </style>
