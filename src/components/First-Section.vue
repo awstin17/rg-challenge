55904672689a03d5b25ca757639f7ed9b2809e1f
awstin17 <austinwiese.acw@gmail.com>
2019-07-17 10:23:34 -0700

    finished code cleanup for first section

--- src/components/First-Section.vue
+++ src/components/First-Section.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="container-fluid p-0">
-    <div style="min-height: 526px" class="row no-gutters">
+    <div id="first-section-row" class="row no-gutters">
       <div
+        id="left-column"
         data-aos="fade-right"
         data-aos-duration="500"
-        class="col-12 col-sm-3 offset-sm-2 d-flex flex-column justify-content-center py-5 px-2"
+        class="col-10 offset-1 col-sm-4 offset-sm-1 col-lg-3 offset-lg-2 d-flex flex-column justify-content-center py-5 py-sm-4"
       >
-        <h1 class="font-weight-bold" style="color: #613476">Lorem Ipsum</h1>
-        <p>Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .</p>
-        <a href style="color: #613476">> Lorem Ipsum</a>
+        <h1 id="title" class="text-secondary">LOREM IPSUM</h1>
+        <p
+          id="description"
+        >Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .</p>
+        <a id="link" href>> Lorem Ipsum</a>
       </div>
-      <div style="min-height: 400px" id="test" class="col-12 col-sm-6 offset-sm-1 pb-5 pb-sm-0"></div>
+      <div id="hikeimg-column" class="col-12 col-sm-6 offset-sm-1"></div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class FirstSection extends Vue {
-  @Prop() private msg!: string;
-}
+export default class FirstSection extends Vue {}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-#test {
+#first-section-row {
+  min-height: 526px;
+}
+
+#hikeimg-column {
   background-image: url("../assets/img-1.jpg");
-  background-position: 50%;
+  background-position: 75%, 50%;
   background-size: cover;
+  min-height: 450px;
 }
 </style>
